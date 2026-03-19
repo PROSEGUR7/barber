247c84cccd0587f89a8181a1528aa44a1d8709c0
brandon <brandondavidbello@gmail.com>
2026-03-19 18:01:03 -0500

    temas

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,23 @@
 'use client'
 
 import * as React from 'react'
+import { usePathname } from 'next/navigation'
 import {
   ThemeProvider as NextThemesProvider,
   type ThemeProviderProps,
 } from 'next-themes'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  const pathname = usePathname()
+  const normalizedPath = (pathname ?? '/').replace(/\/+$/, '') || '/'
+  const shouldForceLight = normalizedPath === '/' || normalizedPath === '/login'
+
+  return (
+    <NextThemesProvider
+      {...props}
+      forcedTheme={shouldForceLight ? 'light' : props.forcedTheme}
+    >
+      {children}
+    </NextThemesProvider>
+  )
 }
